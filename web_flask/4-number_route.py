c313624cad177875d0ce923beb2f58dc05d0aa4d
oladele.david <tdboy59@gmail.com>
2024-05-19 14:42:50 +0100

    4. Is it a number?
    new added

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""""Module to start a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Function to return a string
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function to return a string
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Function to return a string
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Function to return a string
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Function to return a string
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
