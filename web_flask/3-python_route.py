26f358994b05c2a791a66befd464f31073b559ee
oladele.david <tdboy59@gmail.com>
2024-05-19 14:07:11 +0100

    3. Python is cool!
    New task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""""Module to start a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Function to return a string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function to return a string"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Function to return a string
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+def python_is_cool(text):
+    """Function to return a string
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
